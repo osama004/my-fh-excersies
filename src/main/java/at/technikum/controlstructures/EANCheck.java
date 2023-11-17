5bec063e99a2788609eeb7b8f99d77e0f7f2420c
osama004 <osama.madani9@gmail.com>
2023-11-17 10:23:28 +0100

    EANCheck ex done

--- src/main/java/at/technikum/controlstructures/EANCheck.java
+++ src/main/java/at/technikum/controlstructures/EANCheck.java
@@ -1,24 +1,46 @@
 package at.technikum.controlstructures;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EANCheck {
+
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
 
             System.out.print("Enter all 13 digits of an EAN: ");
             long ean = sc.nextLong();
+            Pattern pattern = Pattern.compile("[0-9]{13}");
+            Matcher matcher = pattern.matcher(Long.toString(ean));
+            boolean isValidNumber = matcher.matches();
+            if(!isValidNumber) {
+                System.out.println("Please type a VALID 13 digits!!!");
+                return;
+            }
             long sumEven = 0;
             long sumOdd = 0;
             long decimalPlace = 1000000000000L;
-
-            for (int num = 12; num >= 0 ; num--) {
+            long checkNum = ean % 10;
+            long total = 0;
+            long remainder = 0;
+            System.out.println("i am the checkNum:" + checkNum);
+            for (int num = 0; num <= 12 ; num++) {
                 long oneDigit = ean / decimalPlace % 10L;
-                //System.out.println(oneDigit);
+                System.out.print(oneDigit);
                 if (num % 2 == 0) sumEven += oneDigit;
                 else sumOdd += oneDigit;
                 decimalPlace /= 10;
             }
+            System.out.println("\nSum of EvenNums:" + sumEven);
+            System.out.println("Sum of OddNums:" + sumOdd);
+            total += sumEven * 3 + sumOdd;
+            total -= 1;
+            remainder = total % 10;
+            remainder = checkNum - remainder;
+            System.out.println("I am the final remainder:" + remainder);
+            if (remainder == 0) System.out.println("The EAN is VALID.");
+            else System.out.println("The EAN is NOT VALID.");
 
 
 
